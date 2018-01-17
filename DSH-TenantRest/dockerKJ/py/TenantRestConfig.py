a54f721c1b83179dce8714e9409c2ec652c5db9c
Partapsing, Rakesh <rakesh.partapsing@kpn.com>
2018-01-17 14:39:21 +0100

    removed .crt file

--- DSH-TenantRest/dockerKJ/py/TenantRestConfig.py
+++ DSH-TenantRest/dockerKJ/py/TenantRestConfig.py
@@ -6,14 +6,18 @@ home = expanduser("~")
 absFilePath = os.path.dirname(os.path.abspath(__file__))
 dMountedDiskClient = os.path.join(absFilePath,'mountedDisk')
 fKafkaRunCommand = os.path.join(absFilePath,'kafkaProduceRunCommand.py')
-templatePushMDisk =  os.path.join(absFilePath,'templates','push-mountedDisk.xml')
-templateExecKafkaRunCommand = os.path.join(absFilePath,'templates','executeProduceRunCommand.xml')
+templatePushMDisk =  os.path.join(absFilePath,'templates','push-mountedDisk-pipeline.xml')
+templateExecKafkaRunCommand = os.path.join(absFilePath,'templates','executeProduceRunCommand-pipeline.xml')
+#templatePushMDisk =  os.path.join(absFilePath,'templates','push-mountedDisk.xml')
+#templateExecKafkaRunCommand = os.path.join(absFilePath,'templates','executeProduceRunCommand.xml')
 
 portJenkins = 8084
 portKafka = 9092
 
 ####TENANT SPECIFIC#########
 ipAddress = 'IPADDRESS'
-ipAddressJenkins = ipAddress
 topic = 'TOPIC'
 dMountedDiskHost = r"PATHTOMOUNTEDDISK"
+##JENKINS
+ipAddressJenkins = ipAddress
+crId = 'CREDENTIALSID' # this is the id tag of credentials.xml. This can be found in JENKINS_HOME dir.
